86bd9cb096a18abe65312fc53583e58c21a19680
Md Rezowan Miya <mdrezowanmiya6@gmail.com>
2026-04-02 13:38:58 +0600

    app.js update

--- app.js
+++ app.js
@@ -47,8 +47,6 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // routing setup
 app.use("", loginRouter);
-// app.get("/", (req, res) => {
-//   res.send("Server is running! Welcome to Chat-App 🚀");
 // });
 app.use("/users", usersRouter);
 app.use("/inbox", inboxRouter);
